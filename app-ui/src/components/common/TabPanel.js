2f32e81549a8760441db644a6edfede533bf3d15
haysokhom@nagaworld.com <haysokhom@nagaworld.com>
2019-02-18 14:15:44 +0700

    close tab Pane

--- app-ui/src/components/common/TabPanel.js
+++ app-ui/src/components/common/TabPanel.js
@@ -12,9 +12,7 @@ export default(store,children:Props ) => {
     return class TabPanel extends React.Component {
         constructor(props) {
             super(props);
-            const  TaskListContainer = loadable(() => import('containers/list/TaskListContainer').then(bandle => bandle.default(store)));
-//          const task = <TabPane tab='Task List' key='newTab0' closable={false}><TaskListContainer onNewTabItem={this.onNewTabItem}/></TabPane>
-            const panes = [{id:0, title:'Task List', content: <TaskListContainer onNewTabItem={this.onNewTabItem} /> , key: 'newTab0'}];
+            const panes = [];
             this.state = {
                 activeKey: 'newTab0',
                 panes,
@@ -38,12 +36,16 @@ export default(store,children:Props ) => {
                 }
             });
             const panes = this.state.panes.filter(pane => pane.key !== targetKey);
-            if (lastIndex >= 0 && activeKey === targetKey) {
+            if (lastIndex >= 0 && activeKey === targetKey ) {
                 activeKey = panes[lastIndex].key;
-            }
-            if(panes.length <= 0){
-                activeKey='newTab0';
-            }
+            }else if(activeKey === targetKey){
+                if(panes.length >0){
+                     activeKey = panes[lastIndex+1].key;
+                }else{
+                    activeKey='newTab0';
+                }
+
+            }else{}
             this.setState({ panes, activeKey });
         }
 
@@ -69,7 +71,7 @@ export default(store,children:Props ) => {
         }
 
         render() {
-
+            const  TaskListContainer = loadable(() => import('containers/list/TaskListContainer').then(bandle => bandle.default(store)));
             return (
                 <div>
                     <Tabs
@@ -80,9 +82,11 @@ export default(store,children:Props ) => {
                         onEdit={this.onEdit}
                     >
 
+                        <TabPane tab='Task List' key='newTab0' closable={false}><TaskListContainer onNewTabItem={this.onNewTabItem}/></TabPane>
                         {this.state.panes.map(pane => <TabPane tab={pane.title} key={pane.key}>{pane.content}</TabPane>)}
                     </Tabs>
                 </div>
             );
         }
+    }
 }
