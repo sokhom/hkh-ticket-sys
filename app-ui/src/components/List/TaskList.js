59cc0a84c192e09764eea3abd1e96f56c933e200
haysokhom@nagaworld.com <haysokhom@nagaworld.com>
2019-05-17 18:48:28 +0700

    tasklist

--- app-ui/src/components/List/TaskList.js
+++ app-ui/src/components/List/TaskList.js
@@ -1,24 +1,19 @@
 import React, { PureComponent } from 'react';
 import { List } from 'antd';
-//import InfiniteScroll from 'react-infinite-scroller';
 import Task from './Task';
-import styles from './TaskList.less';
 
 export default(store) => {
     return class TaskList extends React.PureComponent {
         render(){
             return(
-                <div >
-
-                        <List
-                            className={styles.listInfiniteContainer}
-                            header={<div>Header</div>}
-                            footer={<div>Footer</div>}
-                            bordered
-                            dataSource={this.props.data}
-                            renderItem={item => (<List.Item> {Task(store,item,this.props.onNewTabItem)} </List.Item>)}
-                        />
-
+                <div>
+                    <List
+                        header={<div>Header</div>}
+                        footer={<div>Footer</div>}
+                        bordered
+                        dataSource={this.props.data}
+                        renderItem={item => (<List.Item> {Task(store,item,this.props.onNewTabItem)} </List.Item>)}
+                    />
                 </div>
             );
         }
